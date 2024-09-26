4b62169f3ee9af48d1c2fc8c1e4b5fad0171aa06
2013612 <samuelwong2013612@gmail.com>
2024-09-27 01:37:35 +0800

    fix: align kotlin version to 2.0.20

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.compose.compiler)
-    kotlin("plugin.serialization") version "2.0.20"
+    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
