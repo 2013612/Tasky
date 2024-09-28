e8f72b79c40d5273a11259ebf67c4fd1dc4835d7
2013612 <samuelwong2013612@gmail.com>
2024-09-28 10:42:09 +0800

    feat: create login text field

---
+++ androidApp/src/main/java/com/example/tasky/android/login/components/LoginTextField.kt
@@ -0,0 +1,87 @@
+package com.example.tasky.android.login.components
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tasky.android.theme.Light2
+import com.example.tasky.android.theme.LightBlue
+import com.example.tasky.android.theme.MyApplicationTheme
+import com.example.tasky.android.theme.Red
+
+@Composable
+fun LoginTextField(
+    text: String,
+    onTextChange: (String) -> Unit,
+    placeHolder: String,
+    validator: (String) -> Boolean,
+    errorText: String?,
+    modifier: Modifier = Modifier,
+) {
+    OutlinedTextField(
+        text,
+        onTextChange,
+        placeholder = {
+            Text(
+                placeHolder,
+                style = typography.bodySmall,
+                fontWeight = FontWeight.Light,
+                lineHeight = 30.sp,
+            )
+        },
+        textStyle = typography.bodySmall.copy(lineHeight = 30.sp),
+        modifier = modifier,
+        shape = RoundedCornerShape(10.dp),
+        colors =
+            OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Light2,
+                focusedContainerColor = Light2,
+                errorContainerColor = Light2,
+                focusedBorderColor = LightBlue,
+                errorBorderColor = Red,
+            ),
+        trailingIcon =
+            if (validator(text)) {
+                { Icon(Icons.Filled.Check, null, tint = Color.Green) }
+            } else {
+                null
+            },
+        supportingText = { Text(errorText ?: "", style = typography.labelSmall, color = Red) },
+        isError = errorText != null,
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+private fun LoginTextFieldPreview() {
+    MyApplicationTheme {
+        LoginTextField("", {}, "placeholder", { false }, null)
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+private fun LoginTextFieldValidPreview() {
+    MyApplicationTheme {
+        LoginTextField("userName", {}, "placeholder", { true }, null)
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+private fun LoginTextFieldErrorPreview() {
+    MyApplicationTheme {
+        LoginTextField("", {}, "placeholder", { false }, "error Text")
+    }
+}
