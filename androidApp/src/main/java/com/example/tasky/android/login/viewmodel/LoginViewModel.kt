41915472a16a0b26e507f9c3a29431650adb1967
2013612 <samuelwong2013612@gmail.com>
2024-10-01 04:53:52 +0800

    feat: create LoginViewModel

---
+++ androidApp/src/main/java/com/example/tasky/android/login/viewmodel/LoginViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.tasky.android.login.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tasky.android.login.screen.LoginScreenEvent
+import com.example.tasky.android.login.screen.LoginScreenState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class LoginViewModel : ViewModel() {
+    private val _screenStateFlow = MutableStateFlow(LoginScreenState())
+    val screenStateFlow = _screenStateFlow.asStateFlow()
+
+    private val _isLoginSuccessFlow = MutableStateFlow(false)
+    val isLoginSuccessFlow = _isLoginSuccessFlow.asStateFlow()
+
+    fun onEvent(event: LoginScreenEvent) {
+        when (event) {
+            is LoginScreenEvent.OnClickLogin -> login()
+            is LoginScreenEvent.OnClickToSignUp -> {}
+            is LoginScreenEvent.OnStateChange -> _screenStateFlow.value = event.newState
+        }
+    }
+
+    private fun login() {
+        viewModelScope.launch {
+            _isLoginSuccessFlow.value = true
+        }
+    }
+}
