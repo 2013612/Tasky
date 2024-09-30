c1b94fab2f593ad4f303268b1aff1c198577f198
2013612 <samuelwong2013612@gmail.com>
2024-10-01 04:31:45 +0800

    fix: use IconButton instead of clickable

--- androidApp/src/main/java/com/example/tasky/android/login/components/VisibilityTextField.kt
+++ androidApp/src/main/java/com/example/tasky/android/login/components/VisibilityTextField.kt
@@ -1,8 +1,8 @@
 package com.example.tasky.android.login.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -61,26 +61,11 @@ fun VisibilityTextField(
                 unfocusedBorderColor = Color.Transparent,
             ),
         trailingIcon =
-            if (state.isVisible) {
-                {
+            {
+                IconButton(onClick = { onVisibilityChange(!state.isVisible) }) {
                     Icon(
-                        painterResource(R.drawable.visibility),
+                        painterResource(if (state.isVisible) R.drawable.visibility else R.drawable.visibility_off),
                         null,
-                        modifier =
-                            Modifier.clickable {
-                                onVisibilityChange(false)
-                            },
-                    )
-                }
-            } else {
-                {
-                    Icon(
-                        painterResource(R.drawable.visibility_off),
-                        null,
-                        modifier =
-                            Modifier.clickable {
-                                onVisibilityChange(true)
-                            },
                     )
                 }
             },
