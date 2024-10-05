856c1c7e7f9bc50ef0536901da7bd06410011063
2013612 <samuelwong2013612@gmail.com>
2024-10-06 01:12:22 +0900

    feat: add HttpManager

---
+++ shared/src/commonMain/kotlin/com/example/tasky/manager/HttpManager.kt
@@ -0,0 +1,47 @@
+package com.example.tasky.manager
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+internal object HttpManager {
+    private const val TIMEOUT = 60_000L
+
+    val httpClient =
+        HttpClient {
+            install(HttpTimeout) {
+                connectTimeoutMillis = TIMEOUT
+                requestTimeoutMillis = TIMEOUT
+            }
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        prettyPrint = true
+                    },
+                )
+            }
+            install(Logging) {
+                logger =
+                    object : Logger {
+                        override fun log(message: String) {
+                            println("HTTP call $message")
+                        }
+                    }
+                level = LogLevel.ALL
+            }
+
+            defaultRequest {
+                url {
+                    protocol = URLProtocol.HTTPS
+                    host = "tasky.pl-coding.com"
+                }
+            }
+        }
+}
