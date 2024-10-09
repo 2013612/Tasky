1f152d697c905f4d483389d0574dee632452a729
2013612 <samuelwong2013612@gmail.com>
2024-10-10 04:23:02 +0900

    feat: add splashScreen

--- androidApp/src/main/java/com/example/tasky/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/tasky/android/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.tasky.android.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             MyApplicationTheme {
                 Scaffold { innerPadding ->
