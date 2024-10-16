5b888fbb6d4889bc948c712fa05a1acf9d2b02ad
2013612 <samuelwong2013612@gmail.com>
2024-10-17 03:23:43 +0800

    feat: add menu to AgendaCard

--- androidApp/src/main/java/com/example/tasky/android/agenda/components/AgendaCard.kt
+++ androidApp/src/main/java/com/example/tasky/android/agenda/components/AgendaCard.kt
@@ -1,5 +1,6 @@
 package com.example.tasky.android.agenda.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,12 +8,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -36,8 +43,15 @@ import com.example.tasky.model.agenda.Task
 @Composable
 fun AgendaCard(
     agendaItem: AgendaItem,
+    onOpenClick: () -> Unit,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit,
     modifier: Modifier = Modifier,
+    onCircleClick: (() -> Unit)? = null,
 ) {
+    var isMenuOpen by remember {
+        mutableStateOf(false)
+    }
     Card(
         colors = CardDefaults.cardColors().copy(containerColor = getBackgroundColor(agendaItem)),
         shape = shapes.medium,
@@ -45,7 +59,9 @@ fun AgendaCard(
     ) {
         Column {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                IconButton(onClick = {}) {
+                IconButton(onClick = {
+                    onCircleClick?.invoke()
+                }, enabled = onCircleClick != null) {
                     Icon(
                         painter = painterResource(R.drawable.outline_circle_24),
                         contentDescription = null,
@@ -53,12 +69,39 @@ fun AgendaCard(
                     )
                 }
                 Text("Project X", style = typography.headlineMedium, color = getTitleTextColor(agendaItem), modifier = Modifier.weight(1f))
-                IconButton(onClick = {}) {
-                    Icon(
-                        painter = painterResource(R.drawable.outline_more_horiz_24),
-                        contentDescription = null,
-                        tint = getMoreIconColor(agendaItem),
-                    )
+                Box {
+                    IconButton(onClick = {
+                        isMenuOpen = true
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.outline_more_horiz_24),
+                            contentDescription = null,
+                            tint = getMoreIconColor(agendaItem),
+                        )
+                    }
+                    DropdownMenu(
+                        expanded = isMenuOpen,
+                        onDismissRequest = { isMenuOpen = false },
+                    ) {
+                        DropdownMenuItem(text = {
+                            Text("Open")
+                        }, onClick = {
+                            onOpenClick()
+                            isMenuOpen = false
+                        })
+                        DropdownMenuItem(text = {
+                            Text("Edit")
+                        }, onClick = {
+                            onEditClick()
+                            isMenuOpen = false
+                        })
+                        DropdownMenuItem(text = {
+                            Text("Delete")
+                        }, onClick = {
+                            onDeleteClick()
+                            isMenuOpen = false
+                        })
+                    }
                 }
             }
             Row {
@@ -111,6 +154,6 @@ private fun getMoreIconColor(agendaItem: AgendaItem): Color =
 @Composable
 private fun AgendaCardPreview() {
     MyApplicationTheme {
-        AgendaCard(Event(id = 1), modifier = Modifier.height(123.dp))
+        AgendaCard(Event(id = 1), {}, {}, {}, modifier = Modifier.height(123.dp))
     }
 }
