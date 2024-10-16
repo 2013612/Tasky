b5061e58723176c1b0193827b8af42cfc719b18f
2013612 <samuelwong2013612@gmail.com>
2024-10-17 05:14:46 +0800

    feat: add i18n

--- androidApp/src/main/java/com/example/tasky/android/agenda/components/AgendaCard.kt
+++ androidApp/src/main/java/com/example/tasky/android/agenda/components/AgendaCard.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tasky.android.R
@@ -96,19 +97,19 @@ fun AgendaCard(
                         onDismissRequest = { isMenuOpen = false },
                     ) {
                         DropdownMenuItem(text = {
-                            Text("Open")
+                            Text(stringResource(R.string.open))
                         }, onClick = {
                             onOpenClick()
                             isMenuOpen = false
                         })
                         DropdownMenuItem(text = {
-                            Text("Edit")
+                            Text(stringResource(R.string.edit))
                         }, onClick = {
                             onEditClick()
                             isMenuOpen = false
                         })
                         DropdownMenuItem(text = {
-                            Text("Delete")
+                            Text(stringResource(R.string.delete))
                         }, onClick = {
                             onDeleteClick()
                             isMenuOpen = false
