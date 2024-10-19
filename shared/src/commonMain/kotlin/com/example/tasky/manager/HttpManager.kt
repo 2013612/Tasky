94c43986e98dcce30655252a469e7291815e8de9
2013612 <samuelwong2013612@gmail.com>
2024-10-19 20:44:12 +0800

    feat: add Auth plugin to HttpClient for adding Bearer token

--- shared/src/commonMain/kotlin/com/example/tasky/manager/HttpManager.kt
+++ shared/src/commonMain/kotlin/com/example/tasky/manager/HttpManager.kt
@@ -1,8 +1,17 @@
 package com.example.tasky.manager
 
 import com.example.tasky.BuildKonfig.API_KEY
+import com.example.tasky.dataStore.SettingsKey
+import com.example.tasky.dataStore.createSettings
+import com.example.tasky.dataStore.getDataStore
+import com.example.tasky.model.login.LoginResponse
+import com.russhwolf.settings.ExperimentalSettingsApi
+import com.russhwolf.settings.ExperimentalSettingsImplementation
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
@@ -17,6 +26,7 @@ import kotlinx.serialization.json.Json
 internal object HttpManager {
     private const val TIMEOUT = 60_000L
 
+    @OptIn(ExperimentalSettingsImplementation::class, ExperimentalSettingsApi::class)
     val httpClient =
         HttpClient {
             install(Resources)
@@ -39,6 +49,35 @@ internal object HttpManager {
                         }
                     }
                 level = LogLevel.ALL
+            }
+
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                        var accessToken: String
+                        var refreshToken: String
+                        try {
+                            val loginResponseJsonString =
+                                createSettings(
+                                    getDataStore { "" },
+                                ).getStringOrNull(SettingsKey.LOGIN_RESPONSE.name)
+                            val loginResponse =
+                                loginResponseJsonString?.let {
+                                    Json.decodeFromString<LoginResponse>(
+                                        it,
+                                    )
+                                }
+                            accessToken = loginResponse?.accessToken ?: ""
+                            refreshToken = loginResponse?.refreshToken ?: ""
+                        } catch (e: Exception) {
+                            println("cannot load tokens: $e")
+                            accessToken = ""
+                            refreshToken = ""
+                        }
+
+                        BearerTokens(accessToken, refreshToken)
+                    }
+                }
             }
 
             defaultRequest {
