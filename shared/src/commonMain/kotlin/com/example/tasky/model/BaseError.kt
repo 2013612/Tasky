29dbba3f179974323390bb7240b9f9daf8bd6f5f
2013612 <samuelwong2013612@gmail.com>
2024-10-22 02:46:41 +0800

    feat: add http error enums to DataError.Remote

--- shared/src/commonMain/kotlin/com/example/tasky/model/BaseError.kt
+++ shared/src/commonMain/kotlin/com/example/tasky/model/BaseError.kt
@@ -4,6 +4,15 @@ interface BaseError
 
 sealed interface DataError : BaseError {
     enum class Remote : DataError {
+        BAD_REQUEST,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        NOT_FOUND,
+        REQUEST_TIMEOUT,
+        CONFLICT,
+        PAYLOAD_TOO_LARGE,
+        TOO_MANY_REQUESTS,
+        SERVER_ERROR,
         UNKNOWN,
     }
 }
