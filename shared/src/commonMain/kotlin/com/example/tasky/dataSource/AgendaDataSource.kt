01f202a6c7c07887adb4b0a876ccbc1c354eb7d6
2013612 <samuelwong2013612@gmail.com>
2024-10-22 03:07:25 +0800

    feat: add deleteEvent

--- shared/src/commonMain/kotlin/com/example/tasky/dataSource/AgendaDataSource.kt
+++ shared/src/commonMain/kotlin/com/example/tasky/dataSource/AgendaDataSource.kt
@@ -5,9 +5,11 @@ import com.example.tasky.model.BaseError
 import com.example.tasky.model.DataError
 import com.example.tasky.model.ResultWrapper
 import com.example.tasky.model.agenda.Agenda
+import com.example.tasky.model.agenda.EventPath
 import com.example.tasky.model.agenda.GetAgendaResponse
 import com.example.tasky.util.toResult
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.resources.delete
 import io.ktor.client.plugins.resources.get
 import io.ktor.client.request.parameter
 import kotlin.coroutines.cancellation.CancellationException
@@ -23,6 +25,21 @@ class AgendaDataSource(
                 }
 
             response.toResult<GetAgendaResponse>()
+        } catch (e: Exception) {
+            if (e is CancellationException) throw e
+
+            Throwable(e)
+            ResultWrapper.Error(DataError.Remote.UNKNOWN)
+        }
+
+    suspend fun deleteEvent(eventId: String): ResultWrapper<Unit, BaseError> =
+        try {
+            val response =
+                httpClient.delete(EventPath()) {
+                    parameter("eventId", eventId)
+                }
+
+            response.toResult<Unit>()
         } catch (e: Exception) {
             if (e is CancellationException) throw e
 
