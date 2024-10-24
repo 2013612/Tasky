764576052970462f6857a5837b26dff96ad7bf84
2013612 <samuelwong2013612@gmail.com>
2024-10-25 00:44:45 +0800

    feat: add agendaModule to inject AgendaRepository to AgendaViewModel

--- androidApp/src/main/java/com/example/tasky/android/MainApplication.kt
+++ androidApp/src/main/java/com/example/tasky/android/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.example.tasky.android
 
 import android.app.Application
+import com.example.tasky.android.agenda.di.agendaModule
 import com.example.tasky.dataStore.createSettings
 import com.example.tasky.getDataStore
 import com.example.tasky.manager.LoginManager
@@ -8,11 +9,21 @@ import com.example.tasky.manager.loginManager
 import com.example.tasky.repository.LoginRepository
 import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.ExperimentalSettingsImplementation
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class MainApplication : Application() {
     @OptIn(ExperimentalSettingsApi::class, ExperimentalSettingsImplementation::class)
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(
+                agendaModule,
+            )
+        }
         loginManager = LoginManager(createSettings(getDataStore(this)), LoginRepository())
     }
 }
