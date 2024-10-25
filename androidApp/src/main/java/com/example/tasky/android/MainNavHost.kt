267bc2705c7a166458eaa2741b825e044a37de6d
2013612 <samuelwong2013612@gmail.com>
2024-10-26 03:26:21 +0800

    fix: remove unnecessary LaunchedEffect

--- androidApp/src/main/java/com/example/tasky/android/MainNavHost.kt
+++ androidApp/src/main/java/com/example/tasky/android/MainNavHost.kt
@@ -1,17 +1,14 @@
 package com.example.tasky.android
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.example.tasky.android.agenda.AgendaGraph
 import com.example.tasky.android.agenda.agendaGraph
-import com.example.tasky.android.agenda.navigateToAgendaGraph
 import com.example.tasky.android.login.LoginGraph
 import com.example.tasky.android.login.loginGraph
-import com.example.tasky.android.login.navigateToLoginGraph
 import com.example.tasky.manager.loginManager
 
 @Composable
@@ -19,16 +16,6 @@ fun MainNavHost(
     navHostController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
-    LaunchedEffect(true) {
-        loginManager.isLoginFlow.collect { isLogin ->
-            if (isLogin) {
-                navHostController.navigateToAgendaGraph()
-            } else {
-                navHostController.navigateToLoginGraph()
-            }
-        }
-    }
-
     val isLogin = loginManager.isLoginFlow.collectAsState(false).value
 
     NavHost(
