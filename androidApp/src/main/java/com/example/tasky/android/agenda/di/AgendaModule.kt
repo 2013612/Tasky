7e8ad401ef51afa486a7e274c74d274e2dae0d8e
2013612 <samuelwong2013612@gmail.com>
2024-10-25 18:06:04 +0800

    fix: singleOf pattern

--- androidApp/src/main/java/com/example/tasky/android/agenda/di/AgendaModule.kt
+++ androidApp/src/main/java/com/example/tasky/android/agenda/di/AgendaModule.kt
@@ -3,15 +3,13 @@ package com.example.tasky.android.agenda.di
 import com.example.tasky.android.agenda.viewmodel.AgendaViewModel
 import com.example.tasky.repository.AgendaRepository
 import com.example.tasky.repository.IAgendaRepository
-import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val agendaModule =
     module {
-        singleOf<AgendaRepository>(::AgendaRepository) {
-            bind<IAgendaRepository>()
-        }
+        singleOf<AgendaRepository>(::AgendaRepository).bind<IAgendaRepository>()
         viewModelOf(::AgendaViewModel)
     }
