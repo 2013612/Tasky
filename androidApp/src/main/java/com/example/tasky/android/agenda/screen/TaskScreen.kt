3e74febad5360f0b578765fb25fd92e9ff661295
2013612 <samuelwong2013612@gmail.com>
2024-10-26 15:30:11 +0800

    refactor: replace views in TaskScreen by DetailsDeleteSection

--- androidApp/src/main/java/com/example/tasky/android/agenda/screen/TaskScreen.kt
+++ androidApp/src/main/java/com/example/tasky/android/agenda/screen/TaskScreen.kt
@@ -12,16 +12,13 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme.typography
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.tasky.android.agenda.components.DetailsDeleteSection
 import com.example.tasky.android.agenda.components.DetailsDescSection
 import com.example.tasky.android.agenda.components.DetailsHeaderSection
 import com.example.tasky.android.agenda.components.DetailsReminderSection
@@ -29,7 +26,6 @@ import com.example.tasky.android.agenda.components.DetailsStartTimeSection
 import com.example.tasky.android.agenda.components.DetailsTitleSection
 import com.example.tasky.android.agenda.components.DetailsTopBar
 import com.example.tasky.android.theme.Black
-import com.example.tasky.android.theme.Gray
 import com.example.tasky.android.theme.Light
 import com.example.tasky.android.theme.MyApplicationTheme
 import com.example.tasky.model.agenda.AgendaItem
@@ -102,14 +98,7 @@ private fun TaskScreen(
 
                 HorizontalDivider(color = Light)
                 Spacer(Modifier.height(16.dp))
-                TextButton(onClick = {}, modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                    Text(
-                        "DELETE TASK",
-                        style = typography.bodyMedium,
-                        lineHeight = 30.sp,
-                        color = Gray,
-                    )
-                }
+                DetailsDeleteSection(item = state.agendaItem, onClick = {}, modifier = Modifier.align(Alignment.CenterHorizontally))
                 Spacer(Modifier.height(32.dp))
             }
         }
