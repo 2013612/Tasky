5bd8156ee7beb226de0f6892a07d5fe3d12af9cb
2013612 <samuelwong2013612@gmail.com>
2024-10-28 16:10:51 +0800

    feat: add DropdownMenu to DetailsReminderSection

--- androidApp/src/main/java/com/example/tasky/android/agenda/components/DetailsReminderSection.kt
+++ androidApp/src/main/java/com/example/tasky/android/agenda/components/DetailsReminderSection.kt
@@ -1,6 +1,8 @@
 package com.example.tasky.android.agenda.components
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,10 +11,16 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
 import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -24,12 +32,15 @@ import com.example.tasky.android.theme.Gray
 import com.example.tasky.android.theme.Light2
 import com.example.tasky.model.agenda.AgendaItem
 
-enum class ReminderType(val milliSecond: Long) {
-    TEN_MINUTE((10 * 60 * 1000).toLong()),
-    THIRTY_MINUTE((30 * 60 * 1000).toLong()),
-    ONE_HOUR((60 * 60 * 1000).toLong()),
-    SIX_HOUR((6 * 60 * 60 * 1000).toLong()),
-    ONE_DAY((24 * 60 * 60 * 1000).toLong()),
+enum class ReminderType(
+    val milliSecond: Long,
+    @StringRes val stringId: Int,
+) {
+    TEN_MINUTE((10 * 60 * 1000).toLong(), R.string._10_minutes_before),
+    THIRTY_MINUTE((30 * 60 * 1000).toLong(), R.string._30_minutes_before),
+    ONE_HOUR((60 * 60 * 1000).toLong(), R.string._1_hour_before),
+    SIX_HOUR((6 * 60 * 60 * 1000).toLong(), R.string._6_hours_before),
+    ONE_DAY((24 * 60 * 60 * 1000).toLong(), R.string._1_day_before),
 }
 
 @Composable
@@ -39,44 +50,57 @@ fun DetailsReminderSection(
     onReminderSelect: (ReminderType) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
-        Box(
-            modifier =
-                Modifier
-                    .size(30.dp)
-                    .background(Light2),
-        ) {
-            Icon(
-                Icons.Outlined.Notifications,
-                contentDescription = null,
-                tint = Gray,
-                modifier =
-                    Modifier.align(
-                        Alignment.Center,
-                    ),
-            )
-        }
-        Spacer(Modifier.width(8.dp))
-        Text(
-            stringResource(getReminderTextId(item.getStartTime() - item.remindAt)),
-            style = typography.bodySmall,
-            lineHeight = 15.sp,
-            color = Black,
-            modifier = Modifier.weight(1f),
-        )
+    var isMenuOpen by remember {
+        mutableStateOf(false)
+    }
 
-        if (isEdit) {
-            Icon(Icons.AutoMirrored.Outlined.KeyboardArrowRight, contentDescription = null)
+    Box(modifier = modifier) {
+        Row(modifier = Modifier.clickable(enabled = isEdit) { isMenuOpen = true }, verticalAlignment = Alignment.CenterVertically) {
+            Box(
+                modifier =
+                    Modifier
+                        .size(30.dp)
+                        .background(Light2),
+            ) {
+                Icon(
+                    Icons.Outlined.Notifications,
+                    contentDescription = null,
+                    tint = Gray,
+                    modifier =
+                        Modifier.align(
+                            Alignment.Center,
+                        ),
+                )
+            }
+            Spacer(Modifier.width(8.dp))
+            Text(
+                stringResource(getReminderTextId(item.getStartTime() - item.remindAt)),
+                style = typography.bodySmall,
+                lineHeight = 15.sp,
+                color = Black,
+                modifier = Modifier.weight(1f),
+            )
+
+            if (isEdit) {
+                Icon(Icons.AutoMirrored.Outlined.KeyboardArrowRight, contentDescription = null)
+            }
+        }
+
+        DropdownMenu(
+            expanded = isMenuOpen,
+            onDismissRequest = { isMenuOpen = false },
+        ) {
+            ReminderType.entries.map {
+                DropdownMenuItem(text = {
+                    Text(stringResource(it.stringId))
+                }, onClick = {
+                    onReminderSelect(it)
+                    isMenuOpen = false
+                })
+            }
         }
     }
 }
 
 private fun getReminderTextId(time: Long): Int =
-    when (time) {
-        ReminderType.TEN_MINUTE.milliSecond -> R.string._10_minutes_before
-        ReminderType.THIRTY_MINUTE.milliSecond -> R.string._30_minutes_before
-        ReminderType.ONE_HOUR.milliSecond -> R.string._1_hour_before
-        ReminderType.SIX_HOUR.milliSecond -> R.string._6_hours_before
-        ReminderType.ONE_DAY.milliSecond -> R.string._1_day_before
-        else -> R.string._10_minutes_before
-    }
+    ReminderType.entries.firstOrNull { it.milliSecond == time }?.stringId ?: R.string._10_minutes_before
