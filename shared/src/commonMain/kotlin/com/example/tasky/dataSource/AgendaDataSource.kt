8ed091e5328b4cba5647c43bd12ae0469a4850a0
2013612 <samuelwong2013612@gmail.com>
2024-11-05 01:09:04 +0800

    feat: add getEvent

--- shared/src/commonMain/kotlin/com/example/tasky/dataSource/AgendaDataSource.kt
+++ shared/src/commonMain/kotlin/com/example/tasky/dataSource/AgendaDataSource.kt
@@ -130,4 +130,11 @@ class AgendaDataSource(
                 )
             }
         }
+
+    suspend fun getEvent(eventId: String): ResultWrapper<Event, BaseError> =
+        safeCall {
+            httpClient.get(EventPath()) {
+                parameter("eventId", eventId)
+            }
+        }
 }
