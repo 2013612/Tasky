4ff7f35f504922a7cb57137406c8ca5b02491573
2013612 <samuelwong2013612@gmail.com>
2024-11-11 01:20:00 +0800

    feat: add AgendaLocalDataSource with delete agendas

---
+++ shared/src/commonMain/kotlin/com/example/tasky/agenda/data/AgendaLocalDataSource.kt
@@ -0,0 +1,26 @@
+package com.example.tasky.agenda.data
+
+import com.example.tasky.agenda.domain.model.Event
+import com.example.tasky.agenda.domain.model.Reminder
+import com.example.tasky.agenda.domain.model.Task
+import com.example.tasky.database.AppDatabase
+import com.example.tasky.database.database
+import com.example.tasky.database.mapper.toEventEntity
+import com.example.tasky.database.mapper.toReminderEntity
+import com.example.tasky.database.mapper.toTaskEntity
+
+class AgendaLocalDataSource(
+    private val appDatabase: AppDatabase = database,
+) {
+    fun deleteEvent(eventId: String) {
+        appDatabase.eventDao().delete(Event.EMPTY.copy(id = eventId).toEventEntity())
+    }
+
+    fun deleteTask(taskId: String) {
+        appDatabase.taskDao().delete(Task.EMPTY.copy(id = taskId).toTaskEntity())
+    }
+
+    fun deleteReminder(reminderId: String) {
+        appDatabase.reminderDao().delete(Reminder.EMPTY.copy(id = reminderId).toReminderEntity())
+    }
+}
