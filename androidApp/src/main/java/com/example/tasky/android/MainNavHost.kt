1ff9a38ada63607939e314795bc27b9615e6362a
2013612 <samuelwong2013612@gmail.com>
2024-11-14 01:16:58 +0800

    feat: add MainViewModel to MainActivity

--- androidApp/src/main/java/com/example/tasky/android/MainNavHost.kt
+++ androidApp/src/main/java/com/example/tasky/android/MainNavHost.kt
@@ -1,7 +1,6 @@
 package com.example.tasky.android
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -9,17 +8,13 @@ import com.example.tasky.android.agenda.AgendaGraph
 import com.example.tasky.android.agenda.agendaGraph
 import com.example.tasky.android.login.LoginGraph
 import com.example.tasky.android.login.loginGraph
-import com.example.tasky.login.domain.manager.LoginManager
-import org.koin.compose.koinInject
 
 @Composable
 fun MainNavHost(
     navHostController: NavHostController,
+    isLogin: Boolean,
     modifier: Modifier = Modifier,
-    loginManager: LoginManager = koinInject(),
 ) {
-    val isLogin = loginManager.isLoginFlow.collectAsState(false).value
-
     NavHost(
         navController = navHostController,
         startDestination =
