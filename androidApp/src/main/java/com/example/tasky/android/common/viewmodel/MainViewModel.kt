461f2e20aed8e195cd70c0204bca327dae6b7b83
2013612 <samuelwong2013612@gmail.com>
2024-11-15 01:20:50 +0800

    fix: also observe hasConnection for sync agenda

--- androidApp/src/main/java/com/example/tasky/android/common/viewmodel/MainViewModel.kt
+++ androidApp/src/main/java/com/example/tasky/android/common/viewmodel/MainViewModel.kt
@@ -4,22 +4,33 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tasky.agenda.domain.IAgendaRepository
 import com.example.tasky.login.domain.manager.LoginManager
+import dev.tmapps.konnection.Konnection
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class MainViewModel(
     loginManager: LoginManager,
+    konnection: Konnection,
     private val agendaRepository: IAgendaRepository,
 ) : ViewModel() {
     val isLoginFlow =
-        loginManager.isLoginFlow
-            .onEach { isLoggedIn ->
-                if (isLoggedIn) {
-                    viewModelScope.launch {
-                        agendaRepository.syncAgenda()
+        loginManager.isLoginFlow.stateIn(scope = viewModelScope, started = SharingStarted.Eagerly, initialValue = false)
+
+    init {
+        viewModelScope.launch {
+            konnection
+                .observeHasConnection()
+                .combine(loginManager.isLoginFlow) { hasConnection, isLogin ->
+                    isLogin && hasConnection
+                }.collect {
+                    if (it) {
+                        viewModelScope.launch {
+                            agendaRepository.syncAgenda()
+                        }
                     }
                 }
-            }.stateIn(scope = viewModelScope, started = SharingStarted.Eagerly, initialValue = false)
+        }
+    }
 }
