165264e962a9ca8d628114dc6aa8e8e7e392eb54
2013612 <samuelwong2013612@gmail.com>
2024-11-15 02:20:37 +0800

    fix: use static path

--- shared/src/commonMain/kotlin/com/example/tasky/agenda/data/AgendaDataSource.kt
+++ shared/src/commonMain/kotlin/com/example/tasky/agenda/data/AgendaDataSource.kt
@@ -4,17 +4,13 @@ import com.example.tasky.agenda.data.mapper.toCreateEventBody
 import com.example.tasky.agenda.data.mapper.toRemoteReminder
 import com.example.tasky.agenda.data.mapper.toRemoteTask
 import com.example.tasky.agenda.data.mapper.toUpdateEventBody
-import com.example.tasky.agenda.data.model.Agenda
 import com.example.tasky.agenda.data.model.CreateEventBody
-import com.example.tasky.agenda.data.model.EventPath
 import com.example.tasky.agenda.data.model.GetAgendaResponse
 import com.example.tasky.agenda.data.model.GetAttendeeResponse
-import com.example.tasky.agenda.data.model.ReminderPath
 import com.example.tasky.agenda.data.model.RemoteEvent
 import com.example.tasky.agenda.data.model.RemoteReminder
 import com.example.tasky.agenda.data.model.RemoteTask
 import com.example.tasky.agenda.data.model.SyncAgendaBody
-import com.example.tasky.agenda.data.model.TaskPath
 import com.example.tasky.agenda.data.model.UpdateEventBody
 import com.example.tasky.agenda.domain.model.Event
 import com.example.tasky.agenda.domain.model.Reminder
@@ -24,13 +20,13 @@ import com.example.tasky.common.data.model.BaseError
 import com.example.tasky.common.data.util.safeCall
 import com.example.tasky.common.domain.model.ResultWrapper
 import io.ktor.client.HttpClient
-import io.ktor.client.plugins.resources.delete
-import io.ktor.client.plugins.resources.get
-import io.ktor.client.plugins.resources.post
-import io.ktor.client.plugins.resources.put
+import io.ktor.client.request.delete
 import io.ktor.client.request.forms.formData
 import io.ktor.client.request.forms.submitFormWithBinaryData
+import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
@@ -42,42 +38,42 @@ class AgendaDataSource(
 ) {
     suspend fun getAgenda(timeStamp: Long): ResultWrapper<GetAgendaResponse, BaseError> =
         safeCall {
-            httpClient.get(Agenda()) {
+            httpClient.get("/agenda") {
                 parameter("time", timeStamp)
             }
         }
 
     suspend fun deleteEvent(eventId: String): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.delete(EventPath()) {
+            httpClient.delete("/event") {
                 parameter("eventId", eventId)
             }
         }
 
     suspend fun deleteTask(taskId: String): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.delete(TaskPath()) {
+            httpClient.delete("/task") {
                 parameter("taskId", taskId)
             }
         }
 
     suspend fun deleteReminder(reminderId: String): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.delete(ReminderPath()) {
+            httpClient.delete("/reminder") {
                 parameter("reminderId", reminderId)
             }
         }
 
     suspend fun updateTask(task: Task): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.put(TaskPath()) {
+            httpClient.put("/task") {
                 setBody(task.toRemoteTask())
             }
         }
 
     suspend fun updateTask(remoteTask: RemoteTask): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.put(TaskPath()) {
+            httpClient.put("/task") {
                 setBody(remoteTask)
             }
         }
@@ -119,42 +115,42 @@ class AgendaDataSource(
 
     suspend fun updateReminder(reminder: Reminder): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.put(ReminderPath()) {
+            httpClient.put("/reminder") {
                 setBody(reminder.toRemoteReminder())
             }
         }
 
     suspend fun updateReminder(remoteReminder: RemoteReminder): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.put(ReminderPath()) {
+            httpClient.put("/reminder") {
                 setBody(remoteReminder)
             }
         }
 
     suspend fun createTask(task: Task): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.post(TaskPath()) {
+            httpClient.post("/task") {
                 setBody(task.toRemoteTask())
             }
         }
 
     suspend fun createTask(remoteTask: RemoteTask): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.post(TaskPath()) {
+            httpClient.post("/task") {
                 setBody(remoteTask)
             }
         }
 
     suspend fun createReminder(reminder: Reminder): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.post(ReminderPath()) {
+            httpClient.post("/reminder") {
                 setBody(reminder.toRemoteReminder())
             }
         }
 
     suspend fun createReminder(remoteReminder: RemoteReminder): ResultWrapper<Unit, BaseError> =
         safeCall {
-            httpClient.post(ReminderPath()) {
+            httpClient.post("/reminder") {
                 setBody(remoteReminder)
             }
         }
@@ -184,21 +180,21 @@ class AgendaDataSource(
 
     suspend fun getEvent(eventId: String): ResultWrapper<RemoteEvent, BaseError> =
         safeCall {
-            httpClient.get(EventPath()) {
+            httpClient.get("/event") {
                 parameter("eventId", eventId)
             }
         }
 
     suspend fun getTask(taskId: String): ResultWrapper<RemoteTask, BaseError> =
         safeCall {
-            httpClient.get(TaskPath()) {
+            httpClient.get("/task") {
                 parameter("taskId", taskId)
             }
         }
 
     suspend fun getReminder(reminderId: String): ResultWrapper<RemoteReminder, BaseError> =
         safeCall {
-            httpClient.get(ReminderPath()) {
+            httpClient.get("/reminder") {
                 parameter("reminderId", reminderId)
             }
         }
