b2af897bdbb5b13cd3d34d9040cb392aaed213f4
2013612 <samuelwong2013612@gmail.com>
2024-11-21 23:25:15 +0800

    feat: add IntentExtension

---
+++ androidApp/src/main/java/com/example/tasky/android/utils/IntentExtension.kt
@@ -0,0 +1,12 @@
+package com.example.tasky.android.utils
+
+import android.content.Intent
+import android.os.Build
+import android.os.Parcelable
+
+inline fun <reified T : Parcelable> Intent.getCompatParcelableExtra(name: String): T? =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableExtra(name, T::class.java)
+    } else {
+        getParcelableExtra(name)
+    }
