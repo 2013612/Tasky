24c46f552b061b30109149fd240cf547d55cd631
2013612 <samuelwong2013612@gmail.com>
2024-11-22 02:09:47 +0800

    feat: createAgendaAlarm when schedule alarm

--- androidApp/src/main/java/com/example/tasky/android/alarm/data/AlarmScheduler.kt
+++ androidApp/src/main/java/com/example/tasky/android/alarm/data/AlarmScheduler.kt
@@ -4,19 +4,29 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import com.example.tasky.alarm.domain.IAlarmRepository
+import com.example.tasky.alarm.domain.model.AgendaAlarm
 import com.example.tasky.android.alarm.domain.IAlarmScheduler
 import com.example.tasky.android.alarm.domain.mapper.toNotificationDataParcelable
 import com.example.tasky.android.alarm.domain.model.NotificationData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toInstant
 import kotlin.time.DurationUnit
 
 class AlarmScheduler(
     private val context: Context,
+    private val alarmRepository: IAlarmRepository,
 ) : IAlarmScheduler {
     private val alarmManager = context.getSystemService(AlarmManager::class.java)
 
     override fun schedule(data: NotificationData) {
+        CoroutineScope(Dispatchers.IO).launch {
+            alarmRepository.createAgendaAlarm(AgendaAlarm(data.agendaId, data.requestCode))
+        }
+
         val intent =
             Intent(context, AlarmReceiver::class.java).apply {
                 putExtra("DATA", data.toNotificationDataParcelable())
--- androidApp/src/main/java/com/example/tasky/android/alarm/di/AlarmModule.kt
+++ androidApp/src/main/java/com/example/tasky/android/alarm/di/AlarmModule.kt
@@ -1,5 +1,7 @@
 package com.example.tasky.android.alarm.di
 
+import com.example.tasky.alarm.data.AlarmRepository
+import com.example.tasky.alarm.domain.IAlarmRepository
 import com.example.tasky.android.alarm.data.AlarmScheduler
 import com.example.tasky.android.alarm.domain.IAlarmScheduler
 import org.koin.core.module.dsl.singleOf
@@ -8,5 +10,6 @@ import org.koin.dsl.module
 
 val alarmModule =
     module {
+        singleOf<AlarmRepository>(::AlarmRepository).bind<IAlarmRepository>()
         singleOf(::AlarmScheduler).bind<IAlarmScheduler>()
     }
