6c6c122b39fd672226aabd679017e2167e638325
2013612 <samuelwong2013612@gmail.com>
2024-11-22 18:45:10 +0800

    feat: toggle BootReceiver according to isLoginFlow

--- androidApp/src/main/java/com/example/tasky/android/common/viewmodel/MainViewModel.kt
+++ androidApp/src/main/java/com/example/tasky/android/common/viewmodel/MainViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.tasky.android.common.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tasky.agenda.domain.IAgendaRepository
+import com.example.tasky.android.common.data.BootReceiver
 import com.example.tasky.dataStore.isLoginFlow
 import dev.tmapps.konnection.Konnection
 import kotlinx.coroutines.flow.SharingStarted
@@ -15,8 +19,29 @@ import kotlinx.coroutines.flow.stateIn
 class MainViewModel(
     konnection: Konnection,
     private val agendaRepository: IAgendaRepository,
-) : ViewModel() {
-    val isLoginStateFlow = isLoginFlow.stateIn(scope = viewModelScope, started = SharingStarted.Eagerly, initialValue = false)
+    application: Application,
+) : AndroidViewModel(application) {
+    val isLoginStateFlow =
+        isLoginFlow
+            .onEach {
+                if (it) {
+                    val receiver = ComponentName(application, BootReceiver::class.java)
+
+                    application.packageManager.setComponentEnabledSetting(
+                        receiver,
+                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                        PackageManager.DONT_KILL_APP,
+                    )
+                } else {
+                    val receiver = ComponentName(application, BootReceiver::class.java)
+
+                    application.packageManager.setComponentEnabledSetting(
+                        receiver,
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP,
+                    )
+                }
+            }.stateIn(scope = viewModelScope, started = SharingStarted.Eagerly, initialValue = false)
 
     init {
         combine(
