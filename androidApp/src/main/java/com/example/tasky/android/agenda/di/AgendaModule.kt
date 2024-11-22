a3ec9e9d700e9cd572b309c393176ad18646d9c7
2013612 <samuelwong2013612@gmail.com>
2024-11-22 18:49:08 +0800

    fix: koin error for AgendaRepository

--- androidApp/src/main/java/com/example/tasky/android/agenda/di/AgendaModule.kt
+++ androidApp/src/main/java/com/example/tasky/android/agenda/di/AgendaModule.kt
@@ -3,13 +3,14 @@ package com.example.tasky.android.agenda.di
 import com.example.tasky.agenda.data.AgendaRepository
 import com.example.tasky.agenda.domain.IAgendaRepository
 import com.example.tasky.android.agenda.viewmodel.AgendaViewModel
-import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val agendaModule =
     module {
-        singleOf<AgendaRepository>(::AgendaRepository).bind<IAgendaRepository>()
+        single<AgendaRepository> {
+            AgendaRepository(get(), get())
+        }.bind<IAgendaRepository>()
         viewModelOf(::AgendaViewModel)
     }
