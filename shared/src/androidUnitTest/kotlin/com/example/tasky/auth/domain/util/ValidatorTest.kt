4d6ee66318542df36dbd94131d79f0a221f48210
2013612 <samuelwong2013612@gmail.com>
2024-11-25 01:16:59 +0800

    feat: add ValidatorTest

---
+++ shared/src/androidUnitTest/kotlin/com/example/tasky/auth/domain/util/ValidatorTest.kt
@@ -0,0 +1,80 @@
+package com.example.tasky.auth.domain.util
+
+import com.google.common.truth.Truth.assertThat
+import kotlin.test.Test
+
+class ValidatorTest {
+    private val validator: Validator = Validator
+
+    @Test
+    fun `test validateEmail`() {
+        val email = "abc@abc.com"
+
+        val actual = validator.validateEmail(email)
+
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `test validateEmail no @`() {
+        val email = "abcabc.com"
+
+        val actual = validator.validateEmail(email)
+
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `test validateEmail no dot`() {
+        val email = "abc@abccom"
+
+        val actual = validator.validateEmail(email)
+
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `test validateName`() {
+        val name = "name name"
+
+        val actual = validator.validateName(name)
+
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `test validatePassword`() {
+        val password = "aA!123456"
+
+        val actual = validator.validatePassword(password)
+
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `test validatePassword no lowercase`() {
+        val password = "AA!123456"
+
+        val actual = validator.validatePassword(password)
+
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `test validatePassword no uppercase`() {
+        val password = "aa!123456"
+
+        val actual = validator.validatePassword(password)
+
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `test validatePassword not enough length`() {
+        val password = "aA123456"
+
+        val actual = validator.validatePassword(password)
+
+        assertThat(actual).isFalse()
+    }
+}
