28b8f5561ba88fbd565db2eea974a4ba476ac4e9
2013612 <samuelwong2013612@gmail.com>
2025-02-26 18:41:25 +0800

    feat(ios): add dummy RegisterScreen

---
+++ iosApp/iosApp/auth/register/RegisterScreen.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterScreen.swift
+//  iosApp
+//
+//  Created by Wong Samuel on 26/2/2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct RegisterScreen: View {
+    var body: some View {
+        VStack(spacing: 0) {}.background(Color.black)
+    }
+}
